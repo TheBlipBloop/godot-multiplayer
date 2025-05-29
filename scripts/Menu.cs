7d98b20a98d70787229017957574d4f2057e4bc9
TheBlipBloop <blipbloopdev@gmail.com>
2025-05-28 19:06:04 -0700

    Fix merge issue with new menu code.

--- scripts/Menu.cs
+++ scripts/Menu.cs
@@ -1,73 +1,70 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class Menu : CanvasLayer
 {
-	protected LineEdit passwordInput = null;
+    protected LineEdit passwordInput = null;
 
-	protected LineEdit ipInput = null;
+    protected LineEdit ipInput = null;
 
-    private String ip {
-        get {
-            return ipInput.Text;
-        }
+    private String ip
+    {
+        get { return ipInput.Text; }
     }
 
-    private String password {
-        get {
-            return passwordInput.Text;
-        }
+    private String password
+    {
+        get { return passwordInput.Text; }
     }
 
-    protected Lobby LobbyInstance  {
-        get {
-            return Lobby.GetLobbyInstance();
-        }
+    protected Lobby LobbyInstance
+    {
+        get { return Lobby.GetLobbyInstance(); }
     }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
         passwordInput = GetNode<LineEdit>("%password");
         ipInput = GetNode<LineEdit>("%ip");
-	}
+    }
 
-	/*********************************************************************************************/
-	/** Button Signals */
+    /*********************************************************************************************/
+    /** Button Signals */
 
-	public void _on_host_button_down()
-	{
-		Error e = Lobby.GetLobbyInstance().StartServer(ipTextEdit.Text);
-		if (e != Error.Ok)
-		{
-			GD.Print(e.ToString());
-		}
-	}
+    public void _on_host_button_down()
+    {
+        Error e = Lobby.GetLobbyInstance().StartServer(ipInput.Text);
+        if (e != Error.Ok)
+        {
+            GD.Print(e.ToString());
+        }
+    }
 
-	public void _on_join_button_down()
-	{
-		Error e = Lobby.GetLobbyInstance().ConnectClient(ipTextEdit.Text);
-		if (e != Error.Ok)
-		{
-			GD.Print(e.ToString());
-		}
-	}
+    public void _on_join_button_down()
+    {
+        Error e = Lobby.GetLobbyInstance().ConnectClient(ipInput.Text);
+        if (e != Error.Ok)
+        {
+            GD.Print(e.ToString());
+        }
+    }
 
-	public void _on_exit_button_down()
-	{
-		Exit();
-	}
+    public void _on_exit_button_down()
+    {
+        Exit();
+    }
 
-	public void _on_disconnect_button_down()
-	{
-		LobbyInstance.Disconnect();
-	}
+    public void _on_disconnect_button_down()
+    {
+        LobbyInstance.Disconnect();
+    }
 
-	/*********************************************************************************************/
-	/** Menu */
+    /*********************************************************************************************/
+    /** Menu */
 
-	private void Exit()
-	{
-		GetTree().Quit(0);
-	}
+    private void Exit()
+    {
+        GetTree().Quit(0);
+    }
 }
